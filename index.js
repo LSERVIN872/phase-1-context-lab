4db991ef558dfc252228107cb8fcb1dd5d2ab529
luis <servinluis21@yahoo.com>
2023-03-14 13:11:33 -0400

    complete

--- index.js
+++ index.js
@@ -18,6 +18,84 @@ const allWagesFor = function () {
         return memo + wagesEarnedOnDate.call(this, d)
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
+ 
     return payable
+}
+
+const createEmployeeRecord = arr => {
+  let obj = {};
+  return (obj = {
+    firstName: arr[0],
+    familyName: arr[1],
+    title: arr[2],
+    payPerHour: arr[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  });
+};
+
+const createEmployeeRecords = arr => {
+  let result = [];
+  for (const element of arr) {
+    result.push(createEmployeeRecord(element));
+  }
+  return result;
+};
+
+function createTimeInEvent(dateStamp) {
+  let [date, hour] = dateStamp.split(" ");
+
+  this.timeInEvents.push({
+    type: "TimeIn",
+    hour: parseInt(hour, 10),
+    date
+  });
+
+  return this;
+}
+
+function createTimeOutEvent(dateStamp) {
+  let [date, hour] = dateStamp.split(" ");
+
+  this.timeOutEvents.push({
+    type: "TimeOut",
+    hour: parseInt(hour, 10),
+    date
+  });
+
+  return this;
+}
+
+function hoursWorkedOnDate(date) {
+  let dateTimeIn = this.timeInEvents.find(d => d.date == date);
+  let dateTimeOut = this.timeOutEvents.find(d => d.date == date);
+
+  let timeIn = dateTimeIn.hour;
+  let timeOut = dateTimeOut.hour;
+  let hoursWorked = (timeOut - timeIn) / 100;
+  return hoursWorked;
+}
+
+
+function wagesEarnedOnDate(date) {
+  let hoursWorked = hoursWorkedOnDate.call(this, date);
+
+  let payOwed = hoursWorked * this.payPerHour;
+  return payOwed;
+}
+
+
+
+const findEmployeeByFirstName = (srcArray, firstName) => {
+  let employee = srcArray.find(e => (e.firstName = firstName));
+  return employee;
+};
+
+function calculatePayroll(array) {
+  let grandTotal = 0;
+  for (const employee of array) {
+    grandTotal += allWagesFor.call(employee);
+  }
+  return grandTotal;
 }
 
